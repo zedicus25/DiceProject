225624afb6e9e7ddeffb039e96e03d3828de796d
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2021-12-18 11:53:09 +0200

    Called the required methods in main

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,10 @@
 ﻿#include <iostream>
-
+#include "c_DiceSet.h"
 int main()
 {
+	srand(time(NULL));
+	c_DiceSet set(5);
+	std::cout << set;
+	std::cout << "Total amount: "<<set.totalAmount() << "\n";
+	std::cout << set[2];
 }
